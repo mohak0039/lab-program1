48b638b8cab4491909c36c41a84cc9ea4ed1df74
mohak0039 <1by23is118@bmsit.in>
2026-05-15 00:53:44 +0530

    Add HelloWorld class with main method

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins and SonarQube");
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println("Sum = " + (x + y));
+    }
+}
